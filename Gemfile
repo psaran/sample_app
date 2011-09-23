8be83679ce67b258e8913950a60424fb90ba3fc3
Phil Saran <phil.saran@aol.com>
2011-09-23 13:28:00 -0700

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '3.1.0.rc8'
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3'
+gem 'gravatar_image_tag'
 
 group :development do
   gem 'rspec-rails'
@@ -16,6 +17,7 @@ group :test do
   gem 'rspec'
   gem 'webrat'
   gem 'spork'
+  gem 'factory_girl_rails'
 end
 
 group :production do
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
@@ -22,4 +23,29 @@ class User < ActiveRecord::Base
             :presence   => true, 
             :format     => { :with => email_regex }, 
             :uniqueness => { :case_sensitive => false }
+            
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password,
+            :presence     => true,
+            :confirmation => true,
+            :length       => { :within => 6..40 }
+            
+  before_save :encrypt_password
+  
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version od
+    # submitted_password
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  private
+    
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+    
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
 end
